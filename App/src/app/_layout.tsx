32af0f74d89fb58ae90fda9b0d852698a76a9b66
creator-dhruv <dhruvvarshney27@gmail.com>
2026-07-15 23:25:55 +0530

    Add custom tab bar for navigation

--- App/src/app/_layout.tsx
+++ App/src/app/_layout.tsx
@@ -1,5 +1,3 @@
-import { Stack } from "expo-router";
-
 import { useFonts } from "expo-font";
 import {
   PlusJakartaSans_400Regular,
@@ -9,7 +7,93 @@ import {
   PlusJakartaSans_800ExtraBold,
 } from "@expo-google-fonts/plus-jakarta-sans";
 
-export default function RootLayout() {
+import Ionicons from "@react-native-vector-icons/ionicons";
+import { Tabs } from "expo-router";
+import { View, Pressable, Text } from "react-native";
+
+function CustomTabBar({ state, descriptors, navigation }: any) {
+  const currentRoute = state.routes[state.index].name;
+
+  if (currentRoute === "plus") {
+    return null;
+  }
+
+  return (
+    <View
+      style={{
+        position: "absolute",
+        left: 40,
+        right: 40,
+        bottom: 20,
+        height: 65,
+        marginHorizontal: 4,
+        backgroundColor: "#ffffffd9",
+        borderRadius: 35,
+        flexDirection: "row",
+        justifyContent:
+          state.index == 0
+            ? "flex-start"
+            : state.index == 1
+              ? "center"
+              : "flex-end",
+        alignItems: "center",
+        paddingHorizontal: 8,
+        elevation: 10,
+      }}
+    >
+      {state.routes.map((route: any, index: number) => {
+        const focused = state.index === index;
+
+        const { options } = descriptors[route.key];
+
+        const label = options.title ?? options.tabBarLabel ?? route.name;
+
+        let iconName: any = "ellipse";
+
+        if (route.name === "index") iconName = "home";
+        if (route.name === "demo") iconName = "people";
+        if (route.name === "plus") iconName = "add-circle";
+
+        return (
+          <Pressable
+            key={route.key}
+            onPress={() => navigation.navigate(route.name)}
+            style={{
+              height: 50,
+              borderRadius: 25,
+              paddingHorizontal: 18,
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+              backgroundColor: focused ? "#000" : "transparent",
+            }}
+          >
+            <Ionicons
+              name={iconName}
+              size={focused ? 24 : 30}
+              color={focused ? "#fff" : "#000"}
+            />
+
+            {focused && (
+              <Text
+                style={{
+                  color: focused ? "#fff" : "#000",
+                  marginLeft: 8,
+                  fontSize: 16,
+                  fontWeight: "600",
+                }}
+              >
+                {label}
+              </Text>
+            )}
+          </Pressable>
+        );
+      })}
+    </View>
+  );
+}
+
+export default function Layout() {
   const [fontsLoaded] = useFonts({
     PlusJakartaSans_400Regular,
     PlusJakartaSans_500Medium,
@@ -22,9 +106,31 @@ export default function RootLayout() {
     return null;
   }
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" options={{ title: "Home" }} />
-      <Stack.Screen name="demo" options={{ title: "Demo" }} />
-    </Stack>
+    <Tabs
+      tabBar={(props) => <CustomTabBar {...props} />}
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Dashboard",
+        }}
+      />
+
+      <Tabs.Screen
+        name="demo"
+        options={{
+          title: "Community",
+        }}
+      />
+      <Tabs.Screen
+        name="plus"
+        options={{
+          title: "Add Approval",
+        }}
+      />
+    </Tabs>
   );
 }
--- App/src/app/index.tsx
+++ App/src/app/index.tsx
@@ -1,26 +1,42 @@
 import Header from "@/components/Header";
 import QuickActionContainer from "@/components/QuickActionContainer";
 import ApprovalCardStack from "@/components/ApprovalCardStack";
-import { ScrollView, StyleSheet } from "react-native";
+import CommunityContainer from "@/components/CommunityContainer";
+import { ScrollView, StyleSheet, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Colors from "@/constants/color";
 
 export default function Index() {
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView>
+      <ScrollView showsVerticalScrollIndicator={false}>
         <Header />
         <ApprovalCardStack />
-        <QuickActionContainer />
+        <View
+          style={{
+            backgroundColor: Colors.surface,
+            borderTopEndRadius: 40,
+            borderTopStartRadius: 40,
+            marginTop: 10,
+            paddingVertical: 20,
+            paddingBottom: 100,
+            elevation: 5,
+            shadowColor: Colors.black,
+          }}
+        >
+          <QuickActionContainer />
+        </View>
+
+        {/* <CommunityContainer /> */}
       </ScrollView>
     </SafeAreaView>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    width: "100%",
     alignItems: "center",
     justifyContent: "flex-start",
-    paddingVertical: 10,
   },
 });
